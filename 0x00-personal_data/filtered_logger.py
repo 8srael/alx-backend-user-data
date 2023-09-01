b8c0500e60837d0314ad3bc60fd086e02d804c93
8srael <kouassiisrael36@gmail.com>
2023-09-01 19:55:44 +0000

    Task 1. Log formatter🆗

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,8 +10,26 @@ import logging
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ returns the log message obfuscated """
+    """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields or []
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incoming log records"""
+        return filter_datum(self.fields, self.REDACTION, super().format(record),
+                            self.SEPARATOR)
+
