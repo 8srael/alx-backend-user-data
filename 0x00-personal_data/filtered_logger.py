fc0f7b0ef0e14f45ec9667725774e8a1a796123e
8srael <kouassiisrael36@gmail.com>
2023-09-01 21:35:12 +0000

    Task 3 , checker viewing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -40,11 +40,12 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """connects to the MySQL database
        using mysql.connector module
     """
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
-    host = os.getenv('PERSONAL_DATA_DB_HOST')
+    db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    return mysql.connector.connect(user=username,
+    return mysql.connector.connect(user=db_user,
+                                   port=3306,
                                    password=password,
                                    host=host,
                                    database=db_name)
