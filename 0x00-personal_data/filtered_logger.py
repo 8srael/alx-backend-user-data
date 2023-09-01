0a5fa8490dbb27be57e0996061d3a9e4986884ce
8srael <kouassiisrael36@gmail.com>
2023-09-01 16:09:00 +0000

    Task 0, correct output fixed✅

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,12 +5,28 @@
 
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
-        obf_message = re.sub(field + "=.*?" + separator,
+        message = re.sub(field + "=.*?" + separator,
                              field + "=" + redaction + separator, message)
-    return obf_message
+    return message
