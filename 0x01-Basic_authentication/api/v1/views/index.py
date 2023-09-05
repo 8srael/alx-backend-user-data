b598509cc6f61be709ae02678eedccff76c2e653
8srael <kouassiisrael36@gmail.com>
2023-09-05 11:43:26 +0000

    Task 1 . pycodestyle fixed✅

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
@@ -32,4 +33,3 @@ def unauthorized() -> str:
       - abort(401)
     """
     abort(401)
-
