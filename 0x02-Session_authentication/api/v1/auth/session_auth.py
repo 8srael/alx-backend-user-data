0b9c0e2b6c44be7ac5122dfec331b9e2e72a6fed
8srael <kouassiisrael36@gmail.com>
2023-09-08 20:01:45 +0000

    Task 2. create a session updated🆗

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,7 @@
 """Session authentication module"""
 
 from api.v1.auth.auth import Auth
-from uuid import uuid4
+import uuid
 
 
 class SessionAuth(Auth):
@@ -17,6 +17,7 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ Returns a User ID based on session ID """
