1b39bc5aca332c21096c058d087045683161f7a0
8srael <kouassiisrael36@gmail.com>
2023-09-09 23:45:05 +0000

    Task 9. session witth expiration⌛

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ if os.getenv('AUTH_TYPE') == 'basic_auth':
 elif os.getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif os.getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
 else:  # os.getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
