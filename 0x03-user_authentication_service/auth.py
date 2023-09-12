bcc5c7b3e9dfe22953d0c5e9c72265cd01baae16
8srael <kouassiisrael36@gmail.com>
2023-09-12 10:54:16 +0000

    Task 5. app register method updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,9 +32,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        raise ValueError('User {} already exists'.format(user.email))
 
     def valid_login(self, email: str, password: str) -> bool:
         """ Checks if a user exists and validates credentials """
